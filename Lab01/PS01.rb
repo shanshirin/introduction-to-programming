297c54f4a4427daba666fae206c985012b1d959c
shanmukh bellana <shanshirin@git.com>
2011-05-02 20:53:47 -0400

    Finished PS01.

--- Lab01/PS01.rb
+++ Lab01/PS01.rb
@@ -4,3 +4,9 @@
 # 1. Asks the user to enter his/her first name.
 # 2. Asks the user to enter his/her last name.
 # 3. Prints out the user's first and last name in a greeting.
+
+puts "Enter your first name "
+first_name= gets.chomp
+puts "Enter your last name"
+last_name= gets.chomp
+puts "Hello "+first_name+" "+last_name
